aa2900aaf847c940ebb59d9f0e578eb7218d361d
Borislav Stanimirov <b@ibob.bg>
2025-04-24 16:52:41 +0300

    chore: initial boilerplate commit

---
+++ cmake/bl_dep.cmake
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 Schelling Point Ventures Inc.
+# SPDX-License-Identifier: MIT
+#
+macro(bl_dep depName)
+    include(bl-deps/${depName})
+endmacro()
---
+++ sandbox/sandbox.cpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 Schelling Point Ventures Inc.
+// SPDX-License-Identifier: MIT
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, sandbox!" << std::endl;
+    return 0;
+}
