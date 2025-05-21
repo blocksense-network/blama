c942975a3397ef6789f21baf0d10c604e63d4aa0
Borislav Stanimirov <b@ibob.bg>
2025-05-21 16:54:29 +0300

    feat: sketching server interface

--- server/code/server/Server.hpp
+++ server/code/server/Server.hpp
@@ -4,6 +4,9 @@
 #pragma once
 #include "api.h"
 #include <memory>
+#include <string>
+#include <vector>
+#include <itlib/ufunction.hpp>
 
 namespace bl::llama {
 
@@ -18,6 +21,28 @@ public:
 
     Server(const Server&) = delete;
     Server& operator=(const Server&) = delete;
+
+    struct CompleteRequestParams {
+        std::string prompt;
+        uint32_t maxTokens = 0;
+        uint32_t seed = 0;
+        std::string suffix;
+        float temperature = 0.7f;
+    };
+
+    struct TokenData {
+        std::string_view tokenStr;
+        uint32_t tokenId = 0;
+        struct LogitData {
+            uint32_t tokenId = 0;
+            float logit = 0;
+        };
+        std::vector<LogitData> logits;
+    };
+
+    void completeText(CompleteRequestParams params, itlib::ufunction<void(std::vector<TokenData>)> cb);
+
+    void verify(CompleteRequestParams req, std::vector<TokenData> resp, itlib::ufunction<void(float)> cb);
 
 private:
     struct Impl;
