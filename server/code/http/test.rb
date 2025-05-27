9f6e667b17d46d9b8ca68f2d641c544ca885d1a8
Plamen Minev <pacominev@gmail.com>
2025-05-27 17:35:17 +0300

    feat: add example for the chat

--- server/code/http/test.rb
+++ server/code/http/test.rb
@@ -23,3 +23,27 @@ puts "verifying..."
 result = Net::HTTP.post URI.parse(SERVER + '/verify_completion'), JSON.generate(verifyReq), 'Content-Type' => 'text/json'
 p result.each_header.to_h
 puts result.body
+
+# chatCompleteReq = {
+#     messages: [
+#         { role: 'system', content: 'You are a helpful assistant.' },
+#         { role: 'user', content: 'Who was the first president of the United States?' }
+#     ],
+#     max_tokens: 100
+# }
+
+# result = Net::HTTP.post URI.parse(SERVER + '/chat/completions'), JSON.generate(chatCompleteReq), 'Content-Type' => 'text/json'
+# p result.each_header.to_h
+# puts result.body
+
+# verifyReq = {
+#     request: chatCompleteReq,
+#     response: JSON::parse(result.body)
+# }
+
+# puts "verifying..."
+# result = Net::HTTP.post URI.parse(SERVER + '/chat/verify_completion'), JSON.generate(verifyReq), 'Content-Type' => 'text/json'
+# p result.each_header.to_h
+# puts result.body
+
+
